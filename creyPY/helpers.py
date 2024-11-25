364e07daa11c77a0dccb96d9fda7faecf90ce147
Conrad <grosserconrad@gmail.com>
2024-11-25 13:14:07 +0100

    fix: fixed random issue (codacy)

--- creyPY/helpers.py
+++ creyPY/helpers.py
@@ -1,4 +1,4 @@
-import random
+import secrets
 import string
 
 
@@ -6,11 +6,11 @@ def create_random_password(length: int = 12) -> str:
     all_characters = string.ascii_letters + string.digits + string.punctuation
 
     password = [
-        random.choice(string.ascii_lowercase),
-        random.choice(string.ascii_uppercase),
-        random.choice(string.digits),
-        random.choice(string.punctuation),
+        secrets.choice(string.ascii_lowercase),
+        secrets.choice(string.ascii_uppercase),
+        secrets.choice(string.digits),
+        secrets.choice(string.punctuation),
     ]
-    password += random.choices(all_characters, k=length - 4)
-    random.shuffle(password)
+    password += [secrets.choice(all_characters) for _ in range(length - 4)]
+    secrets.SystemRandom().shuffle(password)
     return "".join(password)
