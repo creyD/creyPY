959a746e4fbd235fc2a2d151e82bd7498ddd9468
Conrad <grosserconrad@gmail.com>
2025-01-24 08:48:35 +0100

    fix: fixed issue with new date format

--- creyPY/fastapi/models/base.py
+++ creyPY/fastapi/models/base.py
@@ -1,4 +1,5 @@
 import uuid
+from datetime import datetime, timezone
 
 from sqlalchemy import Column, DateTime, String
 from sqlalchemy.dialects.postgresql import UUID
@@ -13,11 +14,7 @@ class Base:
     # Primary key as uuid
     id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
     created_at = Column(DateTime(timezone=True), server_default=func.now())
-    updated_at = Column(
-        DateTime(timezone=True),
-        server_default=func.now(),
-        onupdate=func.now(),
-    )
+    updated_at = Column(DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
     created_by_id = Column(String)
 
     __name__: str
