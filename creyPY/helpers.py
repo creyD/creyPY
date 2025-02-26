6ac609f3f477e6f92b31834e0ddd80c4b9777768
vikynoah <vigneshwaraa.sarangapani@noah.tech>
2025-02-26 17:11:07 +0100

    feat: Add CSV file Helper (#37)

--- creyPY/helpers.py
+++ creyPY/helpers.py
@@ -1,5 +1,7 @@
 import secrets
 import string
+import csv
+from pathlib import Path
 
 
 def create_random_password(length: int = 12) -> str:
@@ -14,3 +16,10 @@ def create_random_password(length: int = 12) -> str:
     password += [secrets.choice(all_characters) for _ in range(length - 4)]
     secrets.SystemRandom().shuffle(password)
     return "".join(password)
+
+def data_to_csv(file: Path, data:list) -> None:
+
+    with file.open(mode="w", newline="", encoding="utf-8") as f:
+        writer = csv.DictWriter(f, fieldnames=data[0].keys(), delimiter=";")
+        writer.writeheader()
+        writer.writerows(data)
